f80cbdd44fd7187059d06e449c2703ce38a8e78e
Tobias Reich <tobias.reich.ich@gmail.com>
2017-07-17 10:10:31 +0200

    Update cache to working with the next version of Rosid

--- src/index.js
+++ src/index.js
@@ -93,11 +93,11 @@ module.exports.out = function(opts) {
 
 /**
  * Attach an array to the function, which contains a list of
- * extensions used by the handler. The array will be used by Rosid for caching purposes.
+ * file patterns used by the handler. The array will be used by Rosid for caching purposes.
  * @public
  */
 module.exports.cache = [
-	'.ejs',
-	'.json',
-	'.js'
+	'**/*.ejs',
+	'**/*.data.json',
+	'**/*.data.js'
 ]
