63517b71474d37921456754b0237b45dd849cf66
Tobias Reich <tobias.reich.ich@gmail.com>
2018-12-20 15:03:02 +0100

    Support for several kinds of data files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/) and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [9.1.0] - 2018-12-20
+
+### New
+
+- Support for several kinds of data files using [require-data](https://github.com/electerious/require-data)
+- Support for data files returning a function
+- Support for data files returning an async function
+
 ## [9.0.0] - 2018-08-25
 
 ### Changed
--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('path')
+const requireData = require('require-data')
 const continuousStealthyRequire = require('continuous-stealthy-require')
 
 /**
@@ -14,29 +15,38 @@ module.exports = async function(dataPath, opts) {
 
 	const environment = (opts != null && opts.optimize === true) ? 'prod' : 'dev'
 
-	const globalData = (() => {
+	const defaultData = {}
+
+	const globalData = (async () => {
 
 		const hasData = opts != null && opts.data != null
 		if (hasData === false) return {}
 
 		const mustRequire = typeof opts.data === 'string'
-		if (mustRequire === true) return continuousStealthyRequire(path.resolve(opts.data))
+		if (mustRequire === true) return requireData(path.resolve(opts.data), continuousStealthyRequire)
 
 		return opts.data
 
 	})()
 
-	const localData = (() => {
+	const localData = (async () => {
 
 		const hasData = dataPath != null
 		if (hasData === false) return {}
 
-		return continuousStealthyRequire(dataPath)
+		return requireData(dataPath, continuousStealthyRequire)
 
 	})()
 
-	return Object.assign({}, globalData, localData, {
+	const additionalData = {
 		environment
-	})
+	}
+
+	return Object.assign(
+		defaultData,
+		await globalData,
+		await localData,
+		additionalData
+	)
 
 }
